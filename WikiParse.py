53271e55038b67a9e98a6d7bd70b405d86bd2402
Evgeny Kuryshev <47185026+hapik2001@users.noreply.github.com>
2019-01-30 23:17:41 +0300

    Update WikiParse.py

--- WikiParse.py
+++ WikiParse.py
@@ -2,11 +2,15 @@ import json
 import wikipediaapi
 
 def parse_page(w):
+#докстринггде?#непопепу!!!
     return len(w.text)
 
 articles = open('articles.txt', encoding='utf-8').read().splitlines()
+#некрасиво #неэкономно
 langs = open('langs.txt', encoding='utf-8').read().splitlines()
+#ещехуже
 res = {lang:([-100] * len(articles)) for lang in langs}
+#спорно
 wiki = wikipediaapi.Wikipedia('en')
 for ind, article in enumerate(articles):
     page = wiki.page(article)
@@ -23,3 +27,4 @@ out = open("wikiData.txt", "w", encoding='utf-8')
 for key, value in res.items():
     out.write(key + ' ' + ' '.join([str(i) for i in value]) + '\n')
 out.close()
+#ивообщеестьконструкцияwith
